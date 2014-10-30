cc64ff36f07f3116c8a7f3e74bb0abdcb93d57e4
natew <natewienert@gmail.com>
2014-10-30 16:55:45 -0700

    Remove dup code in store

--- repo_modules/brawndo/lib/Store.js
+++ repo_modules/brawndo/lib/Store.js
@@ -1,21 +1,15 @@
 var Fluxxor = require('fluxxor');
-var _ = require('lodash-node');
 
 class Store {
   constructor(name, actions) {
     var self = this;
     this.name = name;
-    this.loading = false;
-    this.data = {};
     this.emit = this.noop;
-
-    this.storeActions = {};
-    this.storeActions[`LOAD_${name}`] = this.onLoading;
 
     this.flux = Fluxxor.createStore({
       initialize() {
         self.emit = this.emit;
-        this.bindActions(_.merge(self.storeActions, actions));
+        this.bindActions(actions);
       }
     });
   }
