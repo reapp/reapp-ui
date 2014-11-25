9efc722de7e6de1c81b21366889424333f1d5e0e
natew <natewienert@gmail.com>
2014-11-25 15:43:05 -0800

    Cleanup touchable area

--- repo_modules/ui/helpers/TouchableArea.jsx
+++ repo_modules/ui/helpers/TouchableArea.jsx
@@ -1,6 +1,10 @@
 var React = require('react');
 
 var TouchableArea = React.createClass({
+  propTypes: {
+    scroller: React.PropTypes.function.isRequired
+  },
+
   getDefaultProps() {
     return {
       element: 'div',
@@ -11,33 +15,35 @@ var TouchableArea = React.createClass({
     };
   },
 
+  getTouchTop: touches => touches[0].pageY,
+  getTouchLeft: touches => touches[0].pageX,
+
+  isWithin: (bounds, point) {
+    return [].concat(bounds)
+      .map(bound => point < bound.to && point > bound.from)
+      .reduce((acc, val) => val && acc); // make sure all are true
+  },
+
+  // this function accounts for touchStartBound that are passed in
+  // if touchStart is within those bounds, it begins touchStartActions
   handleTouchStart(e) {
-    if (!this.props.scroller || !this.props.touchable) {
-      return;
-    }
+    if (!this.props.touchable) return;
 
     this.ignoringScroll = false;
     this._initialTouchLeft = this.getTouchLeft(e.touches);
     this._initialTouchTop = this.getTouchTop(e.touches);
 
     if (this.props.touchStartBounds) {
-      if (this.props.touchStartBounds.x) {
-        this.isWithin(this.props.touchStartBounds.x, e.touches[0].pageX, () => {
-          this.touchStartActions(e);
-        });
-      }
+      var xBounds = this.props.touchStartBounds.x;
+      var yBounds = this.props.touchStartBounds.y;
+      var withinX = !xBounds || this.isWithin(xBounds, this.getTouchLeft(touches));
+      var withinY = !yBounds || this.isWithin(yBounds, this.getTouchTop(touches));
+
+      if (withinX && withinY)
+        this.touchStartActions(e);
     }
-    else {
+    else
       this.touchStartActions(e);
-    }
-  },
-
-  getTouchTop(touches) {
-    return touches[0].pageY;
-  },
-
-  getTouchLeft(touches) {
-    return touches[0].pageX;
   },
 
   touchStartActions(e) {
@@ -46,10 +52,16 @@ var TouchableArea = React.createClass({
   },
 
   handleTouchMove(e) {
-    if (!this.props.scroller || !this.props.touchable || this.ignoringScroll) {
-      return;
-    }
+    if (!this.props.touchable || this.ignoringScroll) return;
+    if (this.ignoreDirectionalScroll(e)) return;
 
+    this.props.scroller.doTouchMove(e.touches, e.timeStamp, e.scale);
+    e.preventDefault();
+  },
+
+  // this will ignore scrolls in a certain direction
+  // for now it's very strict
+  ignoreDirectionalScroll(e) {
     if (this.props.ignoreY || this.props.ignoreX) {
       var distanceY = Math.abs(this._initialTouchTop - this.getTouchTop(e.touches));
       var distanceX = Math.abs(this._initialTouchLeft - this.getTouchLeft(e.touches));
@@ -60,22 +72,13 @@ var TouchableArea = React.createClass({
         return;
       }
     }
-
-    this.props.scroller.doTouchMove(e.touches, e.timeStamp, e.scale);
-    e.preventDefault();
   },
 
   handleTouchEnd(e) {
-    if (!this.props.scroller || !this.props.touchable) {
-      return;
-    }
+    if (!this.props.touchable) return;
 
     this.props.scroller.doTouchEnd(e.timeStamp);
     if (this.props.onTouchEnd) this.props.onTouchEnd(e);
-  },
-
-  isWithin(bounds, point, cb) {
-    [].concat(bounds).map(bound => (point < bound.to && point > bound.from) && cb());
   },
 
   render() {
