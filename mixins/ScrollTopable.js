513642de71d1f36699703187fe475a1aa947d373
natew <natewienert@gmail.com>
2015-01-29 07:33:15 -0800

    Scrolltopable fix

--- mixins/ScrollTopable.js
+++ mixins/ScrollTopable.js
@@ -1,13 +1,37 @@
 var React = require('react');
 
-module.exports = {
-  propTypes: {
-    scrollTop: React.PropTypes.number
-  },
+module.exports = function(node) {
+  return {
+    propTypes: {
+      scrollTop: React.PropTypes.oneOf([
+        React.PropTypes.number,
+        React.PropTypes.string
+      ])
+    },
 
-  getDefaultProps() {
-    return {
-      scrollTop: 0
-    };
+    getDefaultProps() {
+      return {
+        scrollTop: 0
+      };
+    },
+
+    componentDidMount() {
+      this.setScrollTop(this.props);
+    },
+
+    componentWillReceiveProps(nextProps) {
+      this.setScrollTop(nextProps);
+    },
+
+    setScrollTop(props) {
+      if (!props.scrollTop)
+        return;
+
+      var scrollTop = props.scrollTop;
+      if (typeof props.scrollTop === 'string')
+        scrollTop = this.getConstant(props.scrollTop);
+
+      this.refs[node].getDOMNode().scrollTop = scrollTop;
+    }
   }
 };
