52e6ea7ceb3b51ef329122af419e94853af290df
natew <natewienert@gmail.com>
2015-02-25 17:00:20 -0800

    scroll state work on ios

--- mixins/ScrollState.js
+++ mixins/ScrollState.js
@@ -2,40 +2,72 @@
 // Must define isScrolling in getInitialState()
 // and run this.scrollListener(node : DOMNode) on componentWillMount();
 
+function getTouchProps(touch) {
+  if (!touch) return {};
+  return {
+    pageX: touch.pageX,
+    pageY: touch.pageY,
+    clientX: touch.clientX,
+    clientY: touch.clientY
+  };
+}
+
 module.exports = {
-  scrollListener(node) {
-    node.addEventListener('scroll', this.startScrollListen.bind(this, node));
+  componentWillUnmount() {
+    clearInterval(this._scrollEndInterval);
+    this._scrollNode.removeEventListener('touchstart', this._scrollHandleTouchStart);
+    this._scrollNode.removeEventListener('touchmove', this._scrollHandleTouchMove);
   },
 
-  startScrollListen(node) {
+  scrollListener(node) {
+    this._scrollNode = node;
+    this._scrollNode.addEventListener('touchstart', this._scrollHandleTouchStart);
+    this._scrollNode.addEventListener('touchmove', this._scrollHandleTouchMove);
+  },
+
+  calculateMovement: function(touch) {
+    return {
+      x: Math.abs(touch.clientX - this._initialTouch.clientX),
+      y: Math.abs(touch.clientY - this._initialTouch.clientY)
+    };
+  },
+
+  _scrollHandleTouchStart() {
+    this._initialTouch = this._lastTouch = getTouchProps(event.touches[0]);
+  },
+
+  _scrollHandleTouchMove() {
+    this._lastTouch = getTouchProps(event.touches[0]);
+    var movement = this.calculateMovement(this._lastTouch);
+
+    if (movement.x > 20 || movement.y > 20)
+      this._setIsScrolling();
+  },
+
+  _setIsScrolling() {
     if (!this.state.isScrolling) {
       this.setState({ isScrolling: true });
-      this.listenForScrollEnd(node);
+      this._scrollCheckIfEnded();
 
       if (this.props.onScrollStart)
-        this.props.onScrollStart(node);
+        this.props.onScrollStart(this._scrollNode);
     }
   },
 
-  listenForScrollEnd(node) {
-    this._lastScrollPositionY = node.scrollTop;
+  _scrollCheckIfEnded() {
+    var lastPosition = this._scrollNode.scrollTop;
 
-    this.scrollEndInterval = setInterval(() => {
-      var top = node.scrollTop;
-
-      if (this.state.isScrolling && top === this._lastScrollPositionY) {
-        clearInterval(this.scrollEndInterval);
+    this._scrollEndInterval = setInterval(() => {
+      var pos = this._scrollNode.scrollTop;
+      if (pos === lastPosition) {
+        clearInterval(this._scrollEndInterval);
         this.setState({ isScrolling: false });
 
         if (this.props.onScrollEnd)
-          this.props.onScrollEnd(node);
+          this.props.onScrollEnd(this._scrollNode);
       }
-
-      this._lastScrollPositionY = top;
-    }, 300);
-  },
-
-  componentWillUnmount() {
-    clearInterval(this.scrollEndInterval);
+      else
+        lastPosition = pos;
+    }, 200);
   }
 }
