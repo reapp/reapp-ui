a374d55116ea3ecbd4b2098343b3366553eb30d0
natew <natewienert@gmail.com>
2015-03-20 23:32:55 -0700

    update all

--- all.js
+++ all.js
@@ -5,16 +5,21 @@
 
 module.exports = {
   // components
+  Alert: require('./components/Alert'),
   Badge: require('./components/Badge'),
   Bar: require('./components/Bar'),
   BarItem: require('./components/BarItem'),
+  Block: require('./components/Block'),
   Button: require('./components/Button'),
-  ButtonRow: require('./components/ButtonRow'),
+  ButtonGroup: require('./components/ButtonGroup'),
   Card: require('./components/Card'),
   CardList: require('./components/CardList'),
+  Chat: require('./components/Chat'),
+  ChatItem: require('./components/ChatItem'),
   Checkbox: require('./components/Checkbox'),
   Dots: require('./components/Dots'),
   Drawer: require('./components/Drawer'),
+  Gallery: require('./components/Gallery'),
   Form: require('./components/Form'),
   Grid: require('./components/Grid'),
   Icon: require('./components/Icon'),
@@ -24,19 +29,21 @@ module.exports = {
   ListItem: require('./components/ListItem'),
   Menu: require('./components/Menu'),
   Modal: require('./components/Modal'),
+  ModalButton: require('./components/ModalButton'),
   Popover: require('./components/Popover'),
   PopoverLink: require('./components/PopoverLink'),
   Radio: require('./components/Radio'),
   SearchBar: require('./components/SearchBar'),
-  Slider: require('./components/Slider'),
+  Swiper: require('./components/Swiper'),
+  TextArea: require('./components/TextArea'),
   Title: require('./components/Title'),
   TitleBar: require('./components/TitleBar'),
 
   // views
   DottedViewList: require('./views/DottedViewList'),
-  DrawerView: require('./views/DrawerView'),
-  FrostedGlassView: require('./views/FrostedGlassView'),
-  LayoutLeftNav: require('./views/LayoutLeftNav'),
   NestedViewList: require('./views/NestedViewList'),
+  DrawerView: require('./views/DrawerView'),
+  LayoutLeftNav: require('./views/LayoutLeftNav'),
+  ViewList: require('./views/ViewList'),
   View: require('./views/View')
 };
