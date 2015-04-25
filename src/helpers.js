ca9258041e061d8b7ad0f0a7ba712a90c9600928
natew <natewienert@gmail.com>
2015-04-25 11:43:42 -0700

    export some helpers to normal place

---
+++ src/helpers.js
@@ -0,0 +1,8 @@
+export default {
+  classnames: require('classnames'),
+  AnimationHelpers: require('./lib/AnimationHelpers'),
+  clone: require('./lib/niceClone'),
+  StyleKeys: require('./lib/StyleKeys'),
+  Animator: require('./lib/Animator'),
+  EasingFunctions: require('./lib/EasingFunctions')
+}
