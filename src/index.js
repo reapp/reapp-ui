532a73b3f2359ccfac4054be0e8b253426111273
natew <natewienert@gmail.com>
2015-11-19 17:47:01 -0800

    remove raf batch

--- src/index.js
+++ src/index.js
@@ -6,13 +6,6 @@ import { normalize, normalizeAll } from './lib/normalizeStyles';
 require('./lib/desktopTouch');
 require('reapp-object-assign');
 
-if (
-  window._FORCE_ENABLE_RAF ||
-  window.process && process.env &&
-    // if in production and not disabling raf batching
-    process.env.NODE_ENV === 'production' && !process.env.DISABLE_RAF)
-  require('reapp-raf-batching');
-
 // Stores constants, animations and styles
 //   see themes/ios/all.js for an example usage.
 
