ade81161672c06571a7beac143e6408bbc28974d
Zach Swanson <Zach.Swanson@LevelUpDevelopment.com>
2016-03-10 15:29:22 -0500

    Added code to prevent an initial checked state animating to false (now does the opposite)

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -16,6 +16,7 @@ module.exports = Component({
   },
 
   getInitialState() {
+    this.enableAnimations = true;
     return {
       checked: this.props.checked || this.props.defaultChecked,
       disabled: this.props.disabled
@@ -33,16 +34,21 @@ module.exports = Component({
   componentWillReceiveProps(nextProps) {
     if (nextProps.checked !== this.state.checked) {
       let checked = !this.state.checked;
-      if(!!!this.props.disabled)
+      if(!!!this.props.disabled) {
+        this.enableAnimations = false;
         this.setState({ checked: checked });
+      }
     }
   },
 
   render() {
-    if (this.state.checked) {
+    if (this.state.checked && this.enableAnimations) {
       this.addStyles('toggle', 'toggleIsChecked');
       this.addStyles('toggleSwitch', 'toggleSwitchIsChecked');
+    } else {
+      this.enableAnimations = true;
     }
+
 
     var { onChange, defaultChecked, checked, ...props } = this.props;
 
