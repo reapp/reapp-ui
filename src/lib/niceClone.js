0250386f624fd6c90405f0dad2fdefe07c5679a0
Zach Swanson <Zach.Swanson@LevelUpDevelopment.com>
2016-04-14 13:08:07 -0400

    LUD-233: Change merge function for the niceClone library

--- src/lib/niceClone.js
+++ src/lib/niceClone.js
@@ -11,7 +11,7 @@ module.exports = function(children, props, keepOriginalProps) {
 
     return React.isValidElement(child) ?
       React.cloneElement(child, keepOriginalProps ?
-        merge(child.props, curProps) :
+        Object.assign({}, child.props, curProps) :
         curProps) :
       child;
   });
