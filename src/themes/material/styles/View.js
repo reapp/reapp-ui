e9e80cf229b67c23e855f3e720eb41da6adccb7b
Alex Merrifield <alex.merrifield@levelupdevelopment.com>
2016-06-13 15:45:56 -0400

    Fixing a few issues with flex: 1 and android.

--- src/themes/material/styles/View.js
+++ src/themes/material/styles/View.js
@@ -17,14 +17,14 @@ export default c => ({
   inner: {
     background: c.viewBG,
     zIndex: 1,
-    flex: 1,
+    flex: '1 1 100%',
     height: '100%',
     position: 'relative'
   },
 
   static: {
     padding: '0px 8px',
-    flex: 1
+    flex: '1 1 100%'
   },
 
   plain: {
