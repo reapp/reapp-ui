4bd07c09b58998572663c679101b9479cba3ff55
Ishank Tandon <ishank.tandon@levelupdevelopment.com>
2016-06-23 11:42:37 -0400

    typeahead styling changes and changes for wrapper

--- src/themes/material/styles/Typeahead.js
+++ src/themes/material/styles/Typeahead.js
@@ -1,6 +1,7 @@
 module.exports = (c) => ({
   self: {
     boxShadow: '0 1px 6px rgba(0,0,0,0.12), 0 1px 4px rgba(0,0,0,0.12)',
-    borderRadius: '2px'
+    borderRadius: '2px',
+    flex: '1 1 auto'
   },
 });
